145762f747f94eddf7e4d5930952d3cd7149507d
Andre Schubert <andre@andres-mbp-2.speedport_w_724v_09011603_05_010>
2018-03-30 23:51:33 +0200

    first commit

---
+++ src/main/java/de/italoecu/webapp/de/italoecu/webapp/model/Brand.java
@@ -0,0 +1,101 @@
+package de.italoecu.webapp.de.italoecu.webapp.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * Car Brand
+ * @author Andre Schubert <andre.schubert78@gmail.com>
+ * @version 1.0
+ */
+@Entity
+public class Brand {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String country;
+    private Integer foundedIn;
+
+    @OneToMany
+    private Set<Model> models = new HashSet<>();
+
+
+    public Brand() {
+    }
+
+    public Brand(String name, String country, Integer foundedIn) {
+        this.name = name;
+        this.country = country;
+        this.foundedIn = foundedIn;
+    }
+
+    public Set<Model> getModels() {
+        return models;
+    }
+
+    public void setModels(Set<Model> models) {
+        this.models = models;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Integer getFoundedIn() {
+        return foundedIn;
+    }
+
+    public void setFoundedIn(Integer foundedIn) {
+        this.foundedIn = foundedIn;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Brand brand = (Brand) o;
+        return Objects.equals(id, brand.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Brand{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", country='" + country + '\'' +
+                ", foundedIn=" + foundedIn +
+                ", models=" + models +
+                '}';
+    }
+}
---
+++ src/main/java/de/italoecu/webapp/de/italoecu/webapp/model/Model.java
@@ -0,0 +1,88 @@
+package de.italoecu.webapp.de.italoecu.webapp.model;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+/**
+ * Car Model
+ * @author Andre Schubert <andre.schubert78@gmail.com>
+ * @version 1.0
+ */
+@Entity
+public class Model {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String year;
+    @OneToOne
+    private Brand brand;
+
+    public Model() {
+    }
+
+    public Model(String name, String year, Brand brand) {
+        this.name = name;
+        this.year = year;
+        this.brand = brand;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Model model = (Model) o;
+        return Objects.equals(id, model.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Model{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", year='" + year + '\'' +
+                ", brand=" + brand +
+                '}';
+    }
+}
